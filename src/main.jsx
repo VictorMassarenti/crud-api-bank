0867e36f6e1dd7fa4f67dcb766294687f00bbc13
Victor Massarenti <victormassarenti@gmail.com>
2023-08-07 12:36:20 -0300

    mvp cadasctrarCliente

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import theme from './theme';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Gerencia from './pages/Gerencia';
-import Clientes from './pages/Clientes';
+import Clientes from './pages/Cliente';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
--- src/pages/components/CadastrarCliente.jsx
+++ src/pages/components/CadastrarCliente.jsx
@@ -5,10 +5,49 @@
   "email": "email@emarcom",
   "senha": "12345" */
 
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import api from "../services/api";
+import { format, parse } from "date-fns";
+
+function parseDate(date) {
+  const dataParse = parse(date, 'yyyy-MM-dd', new Date())
+  const response = format(dataParse, 'dd/MM/yyyy')
+  return response
+}
 
 export default function CadastrarCliente() {
+  const [nome, setNome] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [data_nascimento, setData_nascimento] = useState('')
+  const [telefone, setTelefone] = useState('')
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const [sendForm, setSendForm] = useState(true)
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    api
+      .post("/contas", {
+        nome,
+        cpf,
+        data_nascimento: parseDate(data_nascimento),
+        telefone,
+        email,
+        senha
+      })
+      .then((response) => console.log(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+    setNome('')
+    setCpf('')
+    setData_nascimento('')
+    setTelefone('')
+    setEmail('')
+    setSenha('')
+    setSendForm(false)
+  }
   return (
     <Box
       component="form"
@@ -21,15 +60,53 @@ export default function CadastrarCliente() {
       }}
       noValidate
       autoComplete="off"
+      onSubmit={handleSubmit}
     >
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Nome"
-          defaultValue="Hello World"
-        />
-      </div>
+      <TextField
+        required
+        label="Nome"
+        type="text"
+        onChange={e => setNome(e.target.value)}
+        value={nome}
+      />
+      <TextField
+        required
+        label="CPF"
+        type="text"
+        onChange={e => setCpf(e.target.value)}
+        value={cpf}
+      />
+      <TextField
+        required
+        type="date"
+        onChange={e => setData_nascimento(e.target.value)}
+        value={data_nascimento}
+      />
+      <TextField
+        required
+        label="Telefone"
+        type="text"
+        onChange={e => setTelefone(e.target.value)}
+        value={telefone}
+      />
+      <TextField
+        required
+        label="Email"
+        type="email"
+        onChange={e => setEmail(e.target.value)}
+        value={email}
+      />
+      <TextField
+        required
+        label="Senha"
+        type="password"
+        onChange={e => setSenha(e.target.value)}
+        value={senha}
+      />
+      <br />
+      <Button disabled={!senha} variant="outlined" type="submit">Cadastrar</Button>
+      <br />
+      <Typography hidden={sendForm} color='secondary'>Cliente cadastrado com sucesso!</Typography>
     </Box>
   )
 }
