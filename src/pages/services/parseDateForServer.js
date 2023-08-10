0968fb284bf48bb79eb1433e90ce73cc529f3d6e
Victor Massarenti <victormassarenti@gmail.com>
2023-08-09 22:42:21 -0300

    refact: ajuste idioma do código

--- src/pages/services/parseDateForServer.js
+++ src/pages/services/parseDateForServer.js
@@ -1,7 +1,7 @@
 import { format, parse } from "date-fns";
 
 export default function parseDateForServer(date) {
-  const dataParse = parse(date, 'yyyy-MM-dd', new Date())
-  const response = format(dataParse, 'dd/MM/yyyy')
+  const dateParse = parse(date, 'yyyy-MM-dd', new Date())
+  const response = format(dateParse, 'dd/MM/yyyy')
   return response
 }
